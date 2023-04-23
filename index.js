d88d183bc8eb793ac62f926a115266217dd8ab31
splean101 <101splean@gmail.com>
2023-04-23 11:04:37 +0300

    mongoose

---
+++ index.js
@@ -0,0 +1,3 @@
+import mongoose from "mongoose";
+
+const url = 'mongodb://localhost:27017/shop';
---
+++ model/product.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+const productSchema = new Schema({
+  title: String,
+  price: Number,
+  rating: Number,
+  category: String,
+  brand: String,
+});
+
+const Product = mongoose.model('product', productSchema);
+
+export { Product };
